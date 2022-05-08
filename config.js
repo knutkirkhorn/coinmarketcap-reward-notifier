7e5d606caa7e1c450ab1ed9859268368d6a56d49
Knut Kirkhorn <knutkirk@hotmail.com>
2022-05-08 22:46:35 +0200

    Fix typo

--- config.js
+++ config.js
@@ -4,11 +4,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 // Default is 60 minutes for the wait timeout
-const HOUR_IN_MILLESECONDS = 3600000;
+const HOUR_IN_MILLISECONDS = 3600000;
 
 export default {
     discordWebhookUrl: process.env.DISCORD_WEBHOOK_URL || '',
     discordWebhookId: process.env.DISCORD_WEBHOOK_ID || '',
     discordWebhookToken: process.env.DISCORD_WEBHOOK_TOKEN || '',
-    waitTimeout: process.env.WAIT_TIMEOUT || HOUR_IN_MILLESECONDS
+    waitTimeout: process.env.WAIT_TIMEOUT || HOUR_IN_MILLISECONDS
 };
