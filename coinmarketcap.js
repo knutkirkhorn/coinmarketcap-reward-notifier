cd5341ef0a0cc5c1421a010895fbe0ee1dfd3616
Knut Kirkhorn <knutkirk@hotmail.com>
2023-01-23 19:21:28 +0100

    Use `package-user-agent` to get User-Agent

--- coinmarketcap.js
+++ coinmarketcap.js
@@ -1,30 +1,32 @@
 // eslint-disable-next-line import/no-unresolved
 import got from 'got';
-import httpHeader from './util.js';
+import getPackageUserAgent from 'package-user-agent';
 
 const apiUrl = 'https://api.coinmarketcap.com/shop/v3/product/list';
 
 export async function getLatestRewardCreationDate() {
+    const packageUserAgent = await getPackageUserAgent();
     const {data} = await got.post(apiUrl, {
         json: {
             currentPage: 1,
             pageSize: 1,
             sortField: 'date'
         },
-        headers: httpHeader
+        headers: packageUserAgent
     }).json();
 
     return data.list[0].createDate;
 }
 
 export async function fetchNewRewards(oldCreationDate) {
+    const packageUserAgent = await getPackageUserAgent();
     const {data} = await got.post(apiUrl, {
         json: {
             currentPage: 1,
             pageSize: 99999,
             sortField: 'date'
         },
-        headers: httpHeader
+        headers: packageUserAgent
     }).json();
 
     const rewards = data.list;
--- util.js
+++
@@ -1,5 +0,0 @@
-const httpHeader = {
-    'User-Agent': 'coinmarketcap-reward-notifier (https://github.com/knutkirkhorn/coinmarketcap-reward-notifier)'
-};
-
-export default httpHeader;
